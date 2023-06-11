7b1635d63b28e3b9afb2ebec24164f6efe871a64
user <shafiqrasool104@gmail.com>
2023-06-11 21:03:04 +0500

    Update

--- app/src/main/java/ca/mohawk/foodrecipeapp/MainActivity.java
+++ app/src/main/java/ca/mohawk/foodrecipeapp/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
